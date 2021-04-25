5ab91eb10e38d7e9991b1864514764bd26a23cf9
David Oesch <david.oesch@gmail.com>
2021-04-25 19:43:38 +0200

    added feedback from https://github.com/davidoesch/swisstopoBatchNmerge/pull/3

--- swisstopoBatchNmerge.py
+++ swisstopoBatchNmerge.py
@@ -7,6 +7,9 @@
 # Author:   David Oesch
 #
 #  usage : 
+# TODO: Usage should appear when running the command empty
+# TODO: Parameters in all caps is unusual (parameters are small caps most of the time
+# TODO: Location and product paramenter need to be specified together. This needs to be mentioned
 #           GUI : python swisstopobatchGUI.py 
 #           GUI with proxy: python swisstopobatchGUI.py --PROXY http://proxy_url:proxy_port
 #           CLI python swisstopobatchGUI.py --CSV "C:\Downloads\ch.swisstopo.swissimage-dop10-5H5DQOGd.csv" --noGUI 1 --noMERGE 1 --PROXY http://proxy_url:proxy_port
@@ -18,7 +21,7 @@
 # - GUI selection place
 # - LIDAR las support
 # - error handling
-# - create an exe via Pyinstaller 
+# - Naming Convention: function and Var needs to be consistent
 
 
 import csv, sys
@@ -26,6 +29,7 @@ from sys import exit
 import requests,json
 import json
 from nested_lookup import nested_lookup
+# TODO: requests already imported above. Use that instead of loading another url/request library
 import urllib.request
 import os
 import argparse
@@ -44,8 +48,11 @@ import shutil
 
 
 # List of supported formats
+# TODO: should come from stac
 supportedformats = ['.tif', '.png', '.tiff', '.TIFF']
 # suuported products
+# TODO: hardcoded. You need to adapt the code for every dataset that is added. Add command to scan it and store
+# in a cache locally
 choices = {
     'SWISSIMAGE 10cm': 'ch.swisstopo.swissimage-dop10',
     'Landeskarte 1:10': 'ch.swisstopo.landeskarte-farbe-10',
@@ -65,20 +72,24 @@ nonImageLayers='swissalti'
 # Function for opening the 
 # file explorer window
 def browseFiles():
-    CSV_value = filedialog.askopenfilename(initialdir = "/",title = "Select a File",filetypes = (("Text files","*.csv*"),("all files","*.*")))
+    # In variabel names, be as specific as possible (what it is)
+    CSV_filepath = filedialog.askopenfilename(initialdir = "/",title = "Select a File",filetypes = (("Text files","*.csv*"),("all files","*.*")))
     # Change label contents
-    label_file_explorer.configure(text="PROCESSING: "+CSV_value+" ...")
-    bbox="n"
-    process(CSV_value,bbox)
-    label_file_explorer.configure(text="FINISHED: "+CSV_value+".tif  ")
+    label_file_explorer.configure(text="PROCESSING: "+CSV_filepath+" ...")
+    # No need to create a variable, set some magic string to it and pass it to a function
+    # Be explicit about what you do
+    processCSV(CSV_filepath,None)
+    label_file_explorer.configure(text="FINISHED: "+CSV_filepath+".tif  ")
     
       
 
 # Function to create a CSV for import	
 def createCSV(productname,LLlon,LLlat,URlon,URlat) :
+    # TODO: Happy path programming. Basic error handling should be added. Try blocks.
+    # Robust code/service is the result of well designed  error handling
     #preparing filename
     coords=(LLlon+"_"+LLlat+"_"+URlon+"_"+URlat)
-    CSV_value=os.path.join(homedir,(productname+coords+".csv"))
+    CSV_filepath=os.path.join(homedir,(productname+coords+".csv"))
     
     #gettingitemlist
     itemsrequest = requests.get("https://data.geo.admin.ch/api/stac/v0.9/collections/"+productname+"/items?bbox="+LLlon+","+LLlat+","+URlon+","+URlat) #cal on STAC API
@@ -87,28 +98,34 @@ def createCSV(productname,LLlon,LLlat,URlon,URlat) :
     itemsfiles=(nested_lookup('href', assets))
     
     #edge case _krel_ : swisstopo provides also grey an relief free raster maps ,we go only for krel
+    # TODO: make special cases generic (outsource to dedicated function that is genereic for similar special cases)
+    # in this case something like 'filter_if_contains(list, term)'. There might be even a ready-made python lib/function
+    # for that -> https://www.kite.com/python/answers/how-to-filter-a-list-in-python
     krel=[i for i in itemsfiles if "_krel_" in i]
     if len(krel) != 0 :
         itemsfiles=krel
     
     #edge case swissimage_ : swisstopo provides different spatiale resolution we go for the 0.1 ,we go only for krel
-
+    # TODO: See, you repeat the same code here for another special case
     highres=[i for i in itemsfiles if "_0.1_" in i]
     if len(highres) != 0 :
         itemsfiles=highres
         
     #create temporaryCSV file
-    with open(CSV_value, 'w') as f:
+    with open(CSV_filepath, 'w') as f:
         for item in itemsfiles:
             f.write("%s\n" % item)
-    return(CSV_value)
+    return(CSV_filepath)
 
 def cropRaster(inputRaster,outputRaster,bbox): #bbox = (ULx,ULy,LRx,LRy)
     transformer = Transformer.from_crs("epsg:4326", "epsg:2056")# transform
     bbox95=(transformer.transform(bbox[3],bbox[0]))+(transformer.transform(bbox[1],bbox[2]))
+    # Create variable, then return nothing?
     result=gdal.Translate(outputRaster,inputRaster, projWin = bbox95) 
+    # Just omitting the return statement will return None
     return()
 
+# There might be a better way to handle this
 def show_progress(block_num, block_size, total_size):
     global pbar
     widgets = [ 'Progress: ' , progressbar.Percentage(),  ' ' ,
@@ -124,8 +141,8 @@ def show_progress(block_num, block_size, total_size):
     else:
         pbar.finish()
         pbar = None
-# Warn if there is not enoiugh free space in dir
-def check_tmp_free_space(gettempdir,filename,lines) :
+# Warn if there is not enough free space in dir etc,
+def check_local_system(gettempdir,filename,lines) :
     file_stats = os.stat(filename)
     requiredspaceGB=((file_stats.st_size / (1024 * 1024 * 1024)))*lines
     free_space = shutil.disk_usage(gettempdir).free
@@ -136,7 +153,8 @@ def check_tmp_free_space(gettempdir,filename,lines) :
     if free_space_gb < requiredspaceGB :
         print("!!!!!!!!!!!!!!!!!!!!!   WARNING  !!!!!!!!!!!!!!!!!!!!")
         print(low_space_message)
-        breakpoint()
+        # what is breakpoint()?
+        exit()
         return
     else :
         low_space_message=(str(round(requiredspaceGB,3))+"GB of "+(str(round(free_space_gb,3))+" GB remaining")) 
@@ -146,6 +164,8 @@ def check_tmp_free_space(gettempdir,filename,lines) :
    
 # Function to derive bbox/geometry from Location name
 def LocationGeomBBOX(location) :
+    # This function uses 2 api calls. But using the swissboundaries3d layer, it could be done in one call with the identify and/or find function
+    # Also, very happy path programming
     #BBOX via API call
     apirequest = requests.get("https://api3.geo.admin.ch/rest/services/api/SearchServer?searchText="+location+"&type=locations&origins=gg25&geometryFormat=geojson&sr=4326")
     apiresult = json.loads(apirequest.content)
@@ -176,34 +196,39 @@ def mergeRaster(iteration,merged,temp_merged,filename,name,ordername,filename_ex
         os.remove(temp_merged) #move iniitial
         os.remove(filename)
         os.rename(merged,temp_merged)
-        print((check_tmp_free_space(homedir,temp_merged,1)+ " will be used up for merging the next image together"))
+        print((check_local_system(homedir,temp_merged,1)+ " will be used up for merging the next image together"))
     return
 
+# Name is not specific, uses other naming conventions as functions above
+# In general, different naming conventions are used for functions and variables.
+# BEtter consistency increases readability of code (code is written once, and read
+# 100 times)
 def LocationProduct(Loc,Prod):
     GeomBBOX=LocationGeomBBOX(Loc)
     coords=GeomBBOX[1].replace("BOX(","")
     coords=coords.replace(")","")
     coords=coords.replace(","," ")
     coords=coords.split(" ")  
-    CSV_Value=createCSV(Prod,coords[0],coords[1],coords[2],coords[3])
+    CSV_filepath=createCSV(Prod,coords[0],coords[1],coords[2],coords[3])
     bbox= (coords[0],coords[1],coords[2],coords[3])
-    process(CSV_Value,bbox)
+    processCSV(CSV_filepath,bbox)
     
-def process(CSV_value,geom):	
+# processCSV what?
+def processCSV(CSV_filepath,geom):	
     
-    filecsv=os.path.split(os.path.abspath(CSV_value))# OS path to CSV
+    filecsv=os.path.split(os.path.abspath(CSV_filepath))# OS path to CSV
     #extracting variables
     downloaddir=filecsv[0] #OS path to download
     ordername=filecsv[1] #ordername
     os.chdir(downloaddir) #go to downloadir
     
     #determine number files to download
-    file = open(os.path.normpath(CSV_value))
+    file = open(os.path.normpath(CSV_filepath))
     reader = csv.reader(file)
     lines= len(list(reader))
     
     # For every line in the file do
-    for iteration, url in enumerate(open(os.path.normpath(CSV_value))):
+    for iteration, url in enumerate(open(os.path.normpath(CSV_filepath))):
         # Split on the rightmost / and take everything on the right side of that
         name = str.rstrip(url.rsplit('/', 1)[-1])
         #print(name)
@@ -225,7 +250,8 @@ def process(CSV_value,geom):
             urllib.request.urlretrieve(url, filename,show_progress)
 
             if iteration == 0 :
-                low_space_message=check_tmp_free_space(homedir,filename,lines)
+                # sometimes you print in the function, sometimes outside of it
+                low_space_message=check_local_system(homedir,filename,lines)
                 print(" ")
                 print("---------------------------------------------------------------------")
                 print(low_space_message+ " for downloaded data needed, might take some time")
@@ -235,7 +261,7 @@ def process(CSV_value,geom):
                 mergeRaster(iteration,merged,temp_merged,filename,name,ordername,filename_ext,homedir)
                 
     #rename data
-    if len(geom) == 4 and args.noCROP == 0 and args.noMERGE == 0: #geom has 4 bbox coordinates
+    if geom is not None and len(geom) == 4 and args.noCROP == 0 and args.noMERGE == 0: #geom has 4 bbox coordinates
         print("Ausschneiden ...")
         
         cropRaster(temp_merged,merged,geom)
@@ -256,11 +282,13 @@ def process(CSV_value,geom):
         print(" ")
         print("Ergebnis in "+result)
         os.chdir(homedir)
+    #exit?
     exit
 
 
 #main partition
 #checking arguments
+# doc missing
 parser = argparse.ArgumentParser(description='--h for all options , eg PROXY http://proxy_url:proxy_port')
 parser.add_argument("--CSV")
 parser.add_argument("--URL")
@@ -293,11 +321,13 @@ if args.noGUI == 0 :
     
     window.mainloop()
 
-
+# the below should be in an else statement, because otherwise
+# combination of parameters have behaviour that is hard to
+# understand (call with --noGUI=0 and --CSV=1 will start GUI and
+# after that will do something with CSV
 #CSV swisstopo CSV use case
 if args.CSV is not None :
-    bbox="n"
-    process(args.CSV,bbox)
+    processCSV(args.CSV,None)
 
 #URL use case
 if args.URL is not None :
@@ -307,8 +337,8 @@ if args.URL is not None :
     homedir = os.getcwd() #get home dir
     splitURL=args.URL.split("/")#get productname
     bbox= (coords[0],coords[1],coords[2],coords[3])
-    CSV_Value=createCSV(splitURL[7],coords[0],coords[1],coords[2],coords[3])
-    process(CSV_Value,bbox)
+    CSV_filepath=createCSV(splitURL[7],coords[0],coords[1],coords[2],coords[3])
+    processCSV(CSV_filepath,bbox)
 
 #LOCATION and PRODUCT use case   
 if args.LOCATION is not None and args.PRODUCT is not None :
